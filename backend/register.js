1581db61c5b4e5d107c235713498dd5427e3c55b
Pascu Maria-Antonia-Emanuela <100427829+antoniamae@users.noreply.github.com>
2023-06-12 17:57:06 +0300

    register

--- backend/register.js
+++ backend/register.js
@@ -4,15 +4,32 @@ const path = require('path');
 const url = require('url');
 const querystring = require('querystring');
 const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
 
 const saltRounds = 10;
+
+// Connect to your MongoDB database
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Define a schema for the user collection
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+// Create a mongoose model for the user collection
+const User = mongoose.model('User', userSchema);
 
 const server = http.createServer((req, res) => {
   const { pathname } = url.parse(req.url);
 
-  if (pathname === '/') {
+  if (pathname === '/register' && req.method === 'GET') {
     // Serve the HTML file
-    const htmlFilePath = path.join(__dirname, '..', 'user-account', 'signup.html');
+    const htmlFilePath = path.join(__dirname, '..', 'frontend', 'user-account', 'signup.html');
     fs.readFile(htmlFilePath, (err, data) => {
       if (err) {
         res.statusCode = 500;
@@ -42,8 +59,8 @@ const server = http.createServer((req, res) => {
       // Check if passwords match
       if (password !== confirmPassword) {
         res.statusCode = 400;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Passwords do not match');
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>Passwords do not match</h1>');
         return;
       }
 
@@ -51,8 +68,8 @@ const server = http.createServer((req, res) => {
       bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
         if (err) {
           res.statusCode = 500;
-          res.setHeader('Content-Type', 'text/plain');
-          res.end('Internal Server Error');
+          res.setHeader('Content-Type', 'text/html');
+          res.end('<h1>Internal Server Error</h1>');
           return;
         }
 
@@ -61,13 +78,13 @@ const server = http.createServer((req, res) => {
 
         // Send a response indicating successful registration
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Registration successful');
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>Registration successful</h1>');
       });
     });
   } else if (pathname === '/styles/style-signup.css') {
     // Serve the CSS file
-    const cssFilePath = path.join(__dirname, '..', 'user-account', 'styles', 'style-signup.css');
+    const cssFilePath = path.join(__dirname, '..', 'frontend', 'user-account', 'styles', 'style-signup.css');
     fs.readFile(cssFilePath, (err, data) => {
       if (err) {
         res.statusCode = 500;
@@ -84,12 +101,22 @@ const server = http.createServer((req, res) => {
   }
 });
 
-function saveUserToDatabase(name, email, password) {
-  // Implement your code here to save the user data to the database
-  // This is just a placeholder function
-  console.log('Name:', name);
-  console.log('Email:', email);
-  console.log('Hashed Password:', password);
+function saveUserToDatabase(name, email, hashedPassword) {
+  // Create a new user instance
+  const user = new User({
+    name: name,
+    email: email,
+    password: hashedPassword,
+  });
+
+  // Save the user to the database
+  user.save((err) => {
+    if (err) {
+      console.error('Error saving user:', err);
+    } else {
+      console.log('User saved successfully');
+    }
+  });
 }
 
 server.listen(3000, () => {
