93d99b8e92ca37dd130f8f7a26042aad6f5f9269
Roxana-Dobrica <100201490+Roxana-Dobrica@users.noreply.github.com>
2023-06-13 16:00:06 +0300

    Added Zoo Plan Page

--- backend/router.js
+++ backend/router.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { handleLandingPage, handleLoginPage,handleHomePage, handleZooPlanPage, handleStaticFile } = require('./handler');
+const { handleLandingPage, handleLoginPage,handleHomePage,handleGeneralAnimalPage,handleAllAnimalPage, handleZooPlanPage, handleStaticFile } = require('./handler');
 const { handleLoginRequest } = require('./login');
 
 function router(req, res) {
@@ -18,6 +18,12 @@ function router(req, res) {
   {
     handleHomePage(req,res);
   } 
+  else if (url === '/animals.html'  || url === '/animals') {
+    handleGeneralAnimalPage(req, res);
+  }
+  else if (url === '/all_animals.html'  || url === '/all_animals') {
+      handleAllAnimalPage(req, res);
+  }
   else if (url === '/zooplan' || url === '/zoo-plan.html' || url === '/zoo-plan' || url === '/zoo-plan/zoo-plan.html') {
     handleZooPlanPage(req, res); 
   }
